f11c28a93b191963c58f8ff78460a2503f1fdaec
skyyygal <heeramnakp@gmail.com>
2026-01-03 23:49:33 +0530

    print statement

---
+++ datatype.py
@@ -0,0 +1,4 @@
+#data types: 1. Integer 2. String 3. Float 4. Boolean 5. None
+
+
+#Str - String 
--- firstProgram.py
+++ firstProgram.py
@@ -1,1 +1,26 @@
 print('Hello World')
+print('My Name is Anita.', "I'm 25 years old")
+#Numbers
+print(10)
+print(23+34)
+print(79-23)
+
+#Variable - is a name given to memory location in a program. 
+#var = value
+
+name = 'Anita'
+age = 25
+cgpa = 9.8
+age2 = age
+print(age)
+print('My name is:', name)
+print('My age is:', age)
+print('My CGPA is:', cgpa)
+print(type(name))
+print(type(age))
+print(type(cgpa))
+
+#rules for identifiers. Identifiers can be combination of uppercase and lowercase letters, digits or an underscore(_).
+# Do myVariable, variable_1, variable_for_print all are valid python identifiers. 
+# An identifier cannot start with digits, variable1 is valid, 1variable is not. 
+# Identifier can be any length. 
