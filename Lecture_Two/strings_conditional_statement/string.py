52b2ee9146b43cf47b2c275424bb34d4e2c865f6
skyyygal <heeramnakp@gmail.com>
2026-01-06 11:57:01 +0530

    STRING

---
+++ Lecture_Two/strings_conditional_statement/string.py
@@ -0,0 +1,31 @@
+#String is a data type that stores a sequence of characters. 
+
+"""
+* concatenation 
+"hello" + "world" -> "helloworld"
+
+* length of str
+ len(str)
+
+"""
+
+str1 = "this is a string."
+str2 = 'Anita'
+str3 = """this is a string"""
+hello = 'Hello'
+name = 'Anita'
+str4 = "This is a string. We are creating it in python."
+# next line - escape sequence characters. 
+str4 = "This is a string. \nWe are creating it in python."
+str4 = "This is a string. \tWe are creating it in python."
+print(str4)
+
+# concatenation
+print(hello+name)
+final_str = hello + '' + name
+
+
+# length
+print(len(name))
+
+
