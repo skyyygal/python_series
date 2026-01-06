39f08db93c8ffd0c8bca4d4f68c9548226472679
skyyygal <heeramnakp@gmail.com>
2026-01-06 20:06:39 +0530

    String and conditional Statemnt.

---
+++ Lecture_Two/strings_conditional_statement/conditional_statement.py
@@ -0,0 +1,25 @@
+age = 21
+
+if(age >=18):
+    # print("Can vote and apply for license")
+    print("Can vote")
+    print("Can drive")
+else:
+    print("Not elligible to vote and apply for license")
+
+light ="red"
+if(light =="red"):
+    print("Stop")
+elif(light =="yellow"):
+    print("look")
+elif(light=="green"):
+    print("Start")
+else : 
+    print("Light is broken")
+print("End of the code")
+
+
+    # What's the difference between if an elif? If statemnt is always checked.
+    #  Elif is check once when the if statement is false.
+    # when all the condition is false, else will be executed.  
+    # Indentation => to combine multiple statement. 
--- Lecture_Two/strings_conditional_statement/string.py
+++ Lecture_Two/strings_conditional_statement/string.py
@@ -18,14 +18,68 @@ str4 = "This is a string. We are creating it in python."
 # next line - escape sequence characters. 
 str4 = "This is a string. \nWe are creating it in python."
 str4 = "This is a string. \tWe are creating it in python."
-print(str4)
+# print(str4)
 
 # concatenation
 print(hello+name)
-final_str = hello + '' + name
-
+final_str = hello + ' ' + name
+print(final_str)
 
 # length
-print(len(name))
+# print(len(name))
 
+print(len(final_str))
+
+# Index - Positioning of the characters
+""" 
+str = 'A n i t a' 
+       0 1 2 3 4 
+  """
+
+ch = str2[0]
+str1 = "anita pandey"
+# str1[4] = '@' manimulation cannot happen. It'll throw error
+print(str1)
+# print(ch)
+
+# Slicing - returns the character of the given frm to end index.
+# str[starting_idx : ending_idx]
+# str[:ending_idx] = str[0:ending_idx]
+# str[starting_idx:] = str[starting_idx:ending_idx]
+
+str = "Anita Pandey"
+ch = str [1:4]
+ch = str[:4]
+ch1 = str[0:]
+ch1 = str[0:len(str)]
+
+print(ch)
+print(ch1)
+
+
+# Case 2 - Slicing / Negative Index(special in python)
+# A  P  P  L  E 
+#-5 -4 -3 -2 -1
+
+str = "Apple"
+ch = str[-3:-1]
+print(ch) #pl
+
+# String function 
+'''
+str = "I am a coder."
+str.endsWith("er") #returns true or false
+str.capitalize() #capitalize 1st character. 
+str.replace(old, new) #replace old with new char. 
+str.find(word)
+str.count("am") #count the occorance of substring in string. 
+'''
+
+str = "I am a coder."
+print(str.endswith('er'))
+print(str.capitalize())
+print(str.replace("I am a coder", "I love coding"))
+print(str) #note: the actual string is not replaced
+print(str.find("code"))
+print(str.count('a'))
 
