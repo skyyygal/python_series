c9bf15c0e1fd721be36bf6eb6617415d8a37cda7
skyyygal <heeramnakp@gmail.com>
2026-01-16 04:52:27 +0530

    dict

--- Lecture4/dictionary_sets/dictionary.py
+++ Lecture4/dictionary_sets/dictionary.py
@@ -6,8 +6,15 @@ dict = {
     "name": "Anita",
     "cgpa": "9.6",
     "marks":[98,97,95],
+    "subjects":{"phy": 97, "chem": 98, "math":99}, #nested dictionalry
     "age":(4, 5, 6)
 }
 print(dict)
 print(dict["marks"])
 print(dict["age"][0])
+
+dict["name"] ="Ano"
+dict["Surname"] ="Pandey" 
+print(dict)
+
+null_dict = {}
